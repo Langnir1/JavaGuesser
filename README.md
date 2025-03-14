907c93433ffa3a92a0c2e82e796f59b9ab33a8c9
Langnir1 <joshua.halamka@bsu.edu>
2025-03-14 14:51:02 +0000

    humanGuesser is working correctly with a the answer line needing to be removed

--- README.md
+++ README.md
@@ -28,3 +28,13 @@ Summary, This code brings up a menu where if the user inputs 0, the code ends, 1
 }
 
 <ins>computerGuesser</ins>
+Summary: This class will have the computer guess a number between 0 to 100 while the user inputs "L", "H", "C". L means low, H means high, C means correct.  
+
+- System.out.println("Think of a random number");
+- boolean keepGoing = true
+- int randomNum = (int)(Math.random() * 101);
+- while keepGoing begins
+- System.out.print("(L)ow, (H)igh, (C)orrect: ");
+- String user = input.nextLine();
+- if user.equals("L"){
+- randomNUm = ()
--- guesser.java
+++ guesser.java
@@ -17,13 +17,59 @@ public class guesser{
 			System.out.print("What would you like to do? ");
 
 			String choice = input.nextLine();
-			System.out.print("Test: " + choice);
+			System.out.println("Test: " + choice);
 			
 			if(choice.equals("0")){
 				System.out.println("Bye!");
 				keepGoing = false;
 			}
+
+			else if(choice.equals("1")){
+				System.out.println("Test response, get rid of later");
+				humanGuesser();
+			}
+
+			else if(choice.equals("2")){
+				System.out.println("Test for computerGuesser");
+				//computerGuesser();
+			}
+			else{
+				System.out.println("Please pick an acceptable response");
+			}
 		}
 
+	}
+
+	public static void humanGuesser(){
+		Scanner input = new Scanner(System.in);
+		boolean keepGoing = true;
+		
+		int number = (int)(Math.random() * 101);
+		System.out.println(number);
+		int count = 1;
+		
+		while(keepGoing){
+			System.out.println();
+			System.out.println(count + ") Guess a number between 0 to 100:  ");
+			int guess = input.nextInt();
+			input.nextLine();
+			
+			if(count > 7){
+				System.out.println("You've exceeded 7 tries, you lose.");
+				keepGoing = false;
+			}
+			else if(guess < number){
+				System.out.println("Too low, try again!");
+				count++;
+			}
+			else if(guess > number){
+				System.out.println("Too high, try again");
+				count++;
+			}
+			else{
+				System.out.println("Congradulations! You got it in " + count + " tries!");
+				keepGoing = false;
+			}
+		}
 	}
 }
